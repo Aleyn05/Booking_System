63806d59f09da83b77854a39de7dd8f963696459
Aleyn05 <131854150+Aleyn05@users.noreply.github.com>
2024-09-25 22:37:38 +0800

    Update Navbar.js, App.css and Navbar.css
    Designed the Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../styles/Navbar.css"
 
 function Navbar() {
     return (
         <nav className="navbar">
+            <h1>La Palmeraie</h1>
             <ul className="nav-list">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
