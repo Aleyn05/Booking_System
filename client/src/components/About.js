c2d55c361c5ca9e35fa7f077d7092cde61164ac3
Aleyn05 <131854150+Aleyn05@users.noreply.github.com>
2024-10-07 18:55:32 +0800

    update About.js
    update info

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,6 +4,7 @@ import '../styles/About.css';
 function About() {
     return (
         <div className="about-container">
+            <div className="overlay"></div>
             <h1>About Our Resort</h1>
             <p>Experience luxury and comfort in our beautiful resort.</p>
         </div>
