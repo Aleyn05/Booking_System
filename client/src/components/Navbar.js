2a47cc63660a34eeab28c6f435d31578d68e3b44
Aleyn05 <131854150+Aleyn05@users.noreply.github.com>
2024-10-09 16:17:21 +0800

    Update Navbar.js and Navbar.css
    remove and change some codes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,19 +19,18 @@ function Navbar() {
         <nav className="navbar">
             <Link to="/" className="navbar-name">La Palmeraie</Link>
             <div className="menu-container">
-                <Link to="/booking" className="booking-link">Booking</Link>
                 <div className="hamburger" onClick={toggleMenu}>
                     &#9776; {/* Hamburger icon */}
                 </div>
             </div>
             <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
                 <li><Link to="/offers">Offers</Link></li>
                 <li><Link to="/weddings-events">Weddings & Events</Link></li>
                 <li><Link to="/accommodations">Accommodations</Link></li>
                 <li><Link to="/dining">Dining</Link></li>
                 <li><Link to="/facilities">Facilities</Link></li>
+                <li><Link to="/booking">Booking</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
