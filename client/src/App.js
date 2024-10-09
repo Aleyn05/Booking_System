bdfd9c5cbfd9cd68d5df63ab5075acb512195605
Aleyn05 <131854150+Aleyn05@users.noreply.github.com>
2024-10-09 16:15:06 +0800

    Update App.js
    Remove and change some codes

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import About from './components/About';
 import Offers from './components/Offers';
-import WeddingsEvents from './components/WeddingsEvents';
+import Events from './components/Events';
 import Accommodations from './components/Accommodations';
 import Dining from './components/Dining';
 import Facilities from './components/Facilities';
 import Booking from './components/Booking';
 import Contacts from './components/Contacts';
+
+import Chatbot from './components/Chatbot';
 
 import './styles/App.css';
 
@@ -22,9 +23,8 @@ function App() {
                 <div className="app-container">
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
                         <Route path="/offers" element={<Offers />} />
-                        <Route path="/weddings-events" element={<WeddingsEvents />} />
+                        <Route path="/weddings-events" element={<Events />} />
                         <Route path="/accommodations" element={<Accommodations />} />
                         <Route path="/dining" element={<Dining />} />
                         <Route path="/facilities" element={<Facilities />} />
@@ -32,6 +32,8 @@ function App() {
                         <Route path="/booking" element={<Booking />} />
                     </Routes>
                 </div>
+                <Chatbot />
+                <script id="messenger-widget-b" src="https://cdn.botpenguin.com/website-bot.js" defer>6706201db561b00b97c70a3d,670615db65fb7611293f479c</script>
                 <footer>
                     <p>&copy; 2024 Resort Booking System. All Rights Reserved.</p>
                 </footer>
