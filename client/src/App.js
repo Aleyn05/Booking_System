53b4a2ee69bc5ef88779e6a7495bd999aaffecfa
Aleyn05 <131854150+Aleyn05@users.noreply.github.com>
2024-10-09 16:30:30 +0800

    Update App.js
    Add some comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+
+// import the pages 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Offers from './components/Offers';
 import Events from './components/Events';
-import Accommodations from './components/Accommodations';
-import Dining from './components/Dining';
 import Facilities from './components/Facilities';
 import Booking from './components/Booking';
 import Contacts from './components/Contacts';
 
+// import the integrated API for Chatbot
 import Chatbot from './components/Chatbot';
 
+// import the the style 
 import './styles/App.css';
 
 function App() {
@@ -25,15 +27,12 @@ function App() {
                         <Route path="/" element={<Home />} />
                         <Route path="/offers" element={<Offers />} />
                         <Route path="/weddings-events" element={<Events />} />
-                        <Route path="/accommodations" element={<Accommodations />} />
-                        <Route path="/dining" element={<Dining />} />
                         <Route path="/facilities" element={<Facilities />} />
                         <Route path="/contact" element={<Contacts />} />
                         <Route path="/booking" element={<Booking />} />
                     </Routes>
                 </div>
                 <Chatbot />
-                <script id="messenger-widget-b" src="https://cdn.botpenguin.com/website-bot.js" defer>6706201db561b00b97c70a3d,670615db65fb7611293f479c</script>
                 <footer>
                     <p>&copy; 2024 Resort Booking System. All Rights Reserved.</p>
                 </footer>
